d36020a24c124f81e7d2e3060bffe8c886dbc2d3
April Copes <antdevelopment1@gmail.com>
2018-10-13 13:12:25 -0400

    Second solution for reversing a string with a manual approach and added test cases. Adds a second module exports and imports file into test.js

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,9 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+
+// Solution #1
 function reverse(str) {
 
 //Takes a string and uses the split method to split each character into an array, reverse the order, and joins the result back together.
   return str.split("").reverse().join("");
+
 }
+
+// Solution #2
+function reverse2(str) {
+  let reversed = '';
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
+}
+
+
 module.exports = reverse;
+module.exports = reverse2;
--- reversestring/test.js
+++ reversestring/test.js
@@ -1,4 +1,19 @@
 const reverse = require('./index');
+const reverse2 = require('./index');
+
+test('Reverse function exists', () => {
+  expect(reverse).toBeDefined();
+});
+
+test('Reverse reverses a string', () => {
+  expect(reverse('abcd')).toEqual('dcba');
+});
+
+test('Reverse reverses a string', () => {
+  expect(reverse('  abcd')).toEqual('dcba  ');
+});
+
+// Added test conditions for Solution #2
 
 test('Reverse function exists', () => {
   expect(reverse).toBeDefined();
