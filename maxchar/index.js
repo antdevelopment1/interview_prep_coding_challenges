8e2f7684b6d1a6d4301ff702e92220fe9bf78514
April Copes <antdevelopment1@gmail.com>
2018-10-25 23:40:59 -0400

    Second alternative to sort repeating letters

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,17 +5,15 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+// This does not solve solution. It just sorts our letters into a key and tracks the number of times the number repeats in the keys value
+// Solution 1:
+// 
+// Solution 2:
 function maxChar(str) {
     const string = 'Hello There!';
     const letters = {};
-    // If we don't check with an if condition first and try to say letters[letter] = letters[letter] + 1 before checking to see ig it already exsists
-    // then we will be adding 1 to undefined which results in our keys values being null
-    for (let letter of string) {
-        if (!letters[letter]) {
-            letters[letter] = 1;
-        } else {
-            letters[letter]++;
-        }
+    for (let letter in letters) {
+        letters[letter] = letters[letter] + 1 || 1;
     }
     return letters;
 }
