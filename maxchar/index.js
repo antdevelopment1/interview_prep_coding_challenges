25156286a7e9e1d162b735b38171a3aad8f7fbd5
April Copes <antdevelopment1@gmail.com>
2018-10-25 23:33:13 -0400

    Uses object to find repeating letters

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const string = 'Hello There!';
+    const letters = {};
+    // If we don't check with an if condition first and try to say letters[letter] = letters[letter] + 1 before checking to see ig it already exsists
+    // then we will be adding 1 to undefined which results in our keys values being null
+    for (let letter of string) {
+        if (!letters[letter]) {
+            letters[letter] = 1;
+        } else {
+            letters[letter]++;
+        }
+    }
+    return letters;
+}
+
 
 module.exports = maxChar;
