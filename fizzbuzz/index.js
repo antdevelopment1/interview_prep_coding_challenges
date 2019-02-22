a701eb68443214046e2e6b53e519e09ce89ff9f0
April Copes <antdevelopment1@gmail.com>
2019-02-22 18:54:55 -0500

    simple fizzbuzz problem

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -11,35 +11,52 @@
 //   fizz
 //   4
 //   buzz
+function fizzBuzz(n) {
+    let start = 1;
 
+    while (start <= n) {
+        if (start % 15 === 0) {
+            console.log("fizzbuzz");
+        } else if (start % 3 === 0) {
+            console.log("fizz");
+        } else if (start % 5 === 0) {
+            console.log("buzz");
+        } else {
+            console.log(start);
+        }
+        start += 1;
+    }
+}
+
+fizzBuzz(100)
 // Solution 1:
-function fizzBuzz(n) {
-    for (let i = 1; i <= n; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log('fizzbuzz');
-        } else if (i % 3 === 0) {
-            console.log('fizz');
-        } else if (i % 5 === 0) {
-            console.log('buzz');
-        } else {
-            console.log(i);
-        }
-    }
-}
+// function fizzBuzz(n) {
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log('fizzbuzz');
+//         } else if (i % 3 === 0) {
+//             console.log('fizz');
+//         } else if (i % 5 === 0) {
+//             console.log('buzz');
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
 
-// Solution 2:
-function fizzBuzz(n) {
-    for (let i = 1; i <= n; i++) {
-        if (i % 15 === 0) {
-            console.log('fizzbuzz');
-        } else if (i % 3 === 0) {
-            console.log('fizz');
-        } else if (i % 5 === 0) {
-            console.log('buzz');
-        } else {
-            console.log(i);
-        }
-    }
-}
+// // Solution 2:
+// function fizzBuzz(n) {
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 15 === 0) {
+//             console.log('fizzbuzz');
+//         } else if (i % 3 === 0) {
+//             console.log('fizz');
+//         } else if (i % 5 === 0) {
+//             console.log('buzz');
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
 
 module.exports = fizzBuzz;
