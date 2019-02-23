cdf91a333296cdae77c01486d72007aef2f2174b
April Copes <antdevelopment1@gmail.com>
2019-02-22 23:25:41 -0500

    adds chuck function algorithm

--- chunk/index.js
+++ chunk/index.js
@@ -7,19 +7,45 @@
 // chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
-
 function chunk(array, size) {
-    const chunked = [];
+    // What do we have
+    // An array with five values - [1,2,3,4,5]
 
-    for (let element of array) {
-        const last = chunked[chunked.length - 1];
-        if (!last || last.length === size) {
-            chunked.push([element]);
-        } else  {
-            last.push(element);
+    // What do we want 
+    // An array with with the number of things in an array based on the second value passe
+    // [[1,2,3], [1,2]]
+    const outerArray = [];
+
+    for (let index of array) {
+        const end = outerArray[outerArray.length - 1];
+        if (!end || end.length === size) {
+            outerArray.push([index]);
+        } else {
+            end.push(index);
         }
     }
-    return chunked;
+    return outerArray;
 }
+
+console.log(chunk([1,2,3,4,5], 3))
+
+
+
+
+
+
+// function chunk(array, size) {
+//     const chunked = [];
+
+//     for (let element of array) {
+//         const last = chunked[chunked.length - 1];
+//         if (!last || last.length === size) {
+//             chunked.push([element]);
+//         } else  {
+//             last.push(element);
+//         }
+//     }
+//     return chunked;
+// }
 
 module.exports = chunk;
