aa22d3347883fe6770ac315d023a22817c442d88
April Copes <antdevelopment1@gmail.com>
2019-02-24 17:27:50 -0500

    Adds steps algorithm function

--- steps/index.js
+++ steps/index.js
@@ -1,22 +1,25 @@
-// --- Directions
 // Write a function that accepts a positive number N.
 // The function should console log a step shape
 // with N levels using the # character.  Make sure the
 // step has spaces on the right hand side!
-// --- Examples
-//   steps(2)
-//       '# '
-//       '##'
-//   steps(3)
-//       '#  '
-//       '## '
-//       '###'
-//   steps(4)
-//       '#   '
-//       '##  '
-//       '### '
-//       '####'
 
-function steps(n) {}
+function steps(n) {
+    // From 0 to n iterate over the rows
+    for (let row = 0; row < n; row++) {
+        // Create an empty string
+        let stair = '';
+        // From 0 to N iterate over the columns
+        for (let column = 0; column < n; column++) {
+            if (column <= row) {
+                stair += '#';
+            } else {
+                stair += ' ';
+            }
+        }
+        console.log(stair);
+    }
+}
+
+console.log(steps(10))
 
 module.exports = steps;
